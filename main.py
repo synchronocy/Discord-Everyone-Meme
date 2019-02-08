be989295f96187875188f658e5fc8b76a0ac40a1
John <29266648+synchronocy@users.noreply.github.com>
2019-02-09 01:41:34 +0930

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import discord
 import asyncio
 import datetime
-
+import random
 now = datetime.datetime.now()
-YOUR_TOKEN = "tokenhere"
+YOUR_TOKEN = 'tokenhere'
 lists = []
 client = discord.Client()
-
+stealth = 0
 @client.event
 async def on_ready():
     counter = -1
     print('Logged in as')
-    print('User: '+str(client.user.name))
-    print('UserID: '+str(client.user.id))
-    print('Email: '+str(client.email))
-    print('Member of: ')
+    print('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Member of:')
     for x in client.servers:
         counter += 1
-        print(str(counter)+'. ( '+str(x.id)+' ) '+str(x)+'\n')
+        print(str(counter)+'. ( '+str(x.id)+' ) '+str(x))
         lists.append(x.id)
     print('Date: '+str(now.day)+'-'+str(now.month)+'-'+str(now.year))
     print('------------')
     a = int(input('Select your server you wish to @Everyone.\n:'))
+    b = int(input('Stealth Mode ( your messages when you send them )\n:')) # drunk adding this :/
+    global stealth # cos there are global variables I'm gonna use em >:)))
+    stealth = b
     SERVER = lists[int(a)]
-    await main(SERVER)
+    await main(SERVER,stealth)
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        if message.content == '@everyone':
-            await client.delete_message(message)
+    if stealth == 1:
+        if message.author == client.user:
+            if message.content == '@everyone':
+                await client.delete_message(message)
+    else:
+        return ''
+    
             
-async def main(SERVER):
+async def main(SERVER, stealth):
     while True:
         channellist = []
-        counter = random.randint(1,10)
-        print('Awaiting for '+str(counter)+' Seconds')
-        await asyncio.sleep(int(counter))
+        if stealth == 1:
+            counter = random.randint(1,10)
+            print('Awaiting for '+str(counter)+' Seconds')
+            await asyncio.sleep(int(counter))
         for channel in client.get_server(SERVER).channels:
             if channel.type != discord.ChannelType.text:
                 continue
